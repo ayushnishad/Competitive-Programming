b57d863c599e90ddf703552cbdbc8bea20943100
ayushnishad <ayushnishadcr7@gmail.com>
2020-10-07 22:35:21 +0530

    basic_trie.cpp added

---
+++ Snippets/basic_trie.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct trie {
+	trie* child[26];
+	bool isEnd;
+	trie()
+	{
+		memset(child, 0, sizeof(child));
+		isEnd = 0;
+	}
+};
+
+trie *root; // intialize root before use
+
+void insert_(string s)
+{
+	trie* curr = root;
+	for (char ch : s)
+	{
+		int idx = ch - 'a';
+		if (!curr->child[idx])
+		{
+			curr->child[idx] = new trie;
+		}
+		curr = curr->child[idx];
+	}
+	curr->isEnd = 1;
+}
+
+bool find_(string s)
+{
+	trie* curr = root;
+	for (char ch : s)
+	{
+		int idx = ch - 'a';
+		if (!curr->child[idx])
+			return 0;
+		curr = curr->child[idx];
+	}
+	return curr->isEnd;
+}
+
+int main()
+{
+	root = new trie;
+	int q; cin >> q;
+	while (q--)
+	{
+		string s; cin >> s;
+		insert_(s);
+	}
+	cin >> q;
+	while (q--)
+	{
+		string s; cin >> s;
+		if (find_(s))
+			cout << "Present\n";
+		else
+			cout << "Not\n";
+	}
+	return 0;
+}
